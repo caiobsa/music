91f278629106b9411acf93638ac89731941a91cf
caiobsa <caiobsa@gmail.com>
2023-07-17 23:38:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ import streamlit as st
 
 # Dicionário para mapear notas musicais aos seus campos harmônicos
 CAMPOS_HARMONICOS_MAIOR = {
-    "C": ["C", "D", "E", "F", "G", "A", "B"],
-    "D": ["D", "E", "F#", "G", "A", "B", "C#"],
-    "E": ["E", "F#", "G#", "A", "B", "C#", "D#"],
-    "F": ["F", "G", "A", "A#", "C", "D", "E"],
-    "G": ["G", "A", "B", "C", "D", "E", "F#"],
-    "A": ["A", "B", "C#", "D", "E", "F#", "G#"],
-    "B": ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+    "C": ["C", "Dm", "Em", "F", "G", "Am", "Bdim"],
+    "D": ["D", "Em", "F#m", "G", "A", "Bm", "C#dim"],
+    "E": ["E", "F#m", "G#m", "A", "B", "C#m", "D#dim"],
+    "F": ["F", "Gm", "Am", "Bb", "C", "Dm", "Edim"],
+    "G": ["G", "Am", "Bm", "C", "D", "Em", "F#dim"],
+    "A": ["A", "Bm", "C#m", "D", "E", "F#m", "G#dim"],
+    "B": ["B", "C#m", "D#m", "E", "F#", "G#m", "A#dim"],
 }
 
 CAMPOS_HARMONICOS_MENOR = {
-    "C": ["C", "D", "D#", "F", "G", "G#", "A#"],
-    "D": ["D", "E", "F", "G", "A", "A#", "C"],
-    "E": ["E", "F#", "G", "A", "B", "C", "D"],
-    "F": ["F", "G", "G#", "A#", "C", "C#", "D#"],
-    "G": ["G", "A", "A#", "C", "D", "D#", "F"],
-    "A": ["A", "B", "C", "D", "E", "F", "G"],
-    "B": ["B", "C#", "D", "E", "F#", "G", "A"],
+    "C": ["Cm", "Ddim", "Eb", "Fm", "Gm", "Ab", "Bb"],
+    "D": ["Dm", "Edim", "F", "Gm", "Am", "Bb", "C"],
+    "E": ["Em", "F#dim", "G", "Am", "Bm", "C", "D"],
+    "F": ["Fm", "Gdim", "Ab", "Bbm", "Cm", "Db", "Eb"],
+    "G": ["Gm", "Adim", "Bb", "Cm", "Dm", "Eb", "F"],
+    "A": ["Am", "Bdim", "C", "Dm", "Em", "F", "G"],
+    "B": ["Bm", "C#dim", "D", "Em", "F#m", "G", "A"],
 }
 
 CAMPOS_HARMONICOS_SETIMA_MAIOR = {
